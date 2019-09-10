9bb3cfa476dfc4972d305bf9fd82dbd8736c7ac1
Ilya Korobkov <serverprom@yandex.ru>
2019-09-10 15:08:31 +0300

    Added some additional debug output

--- usr/src/main.ts
+++ usr/src/main.ts
@@ -18,13 +18,17 @@ sw.update = function (delta: number) {
     // @ts-ignore
     // console.log(Vector2.staticFunction(), Vector2.staticVar);
     console.log("Greetings from TypeScript!");
+    console.log("From JS: creating Vector2");
     let a = new sw.Vector2(-3, -4);
-    console.log(a.length());
+    console.log("From JS: calling Vector2::length function");
+    console.log("From JS: " + a.length());
+    console.log("From JS: calling 'add' on Vector2");
     a.add(1);
-    console.log(a.x, a.y);
-    console.log(a.length());
+    console.log("From JS: " + a.x, a.y);
+    console.log("From JS: " + a.length());
+    console.log("From JS: creating Vector2 by calling static function 'add' on Vector2");
     let b = sw.Vector2.add(a, a);
-    console.log(b.x, b.y);
+    console.log("From JS: " + b.x, b.y);
     // console.log(z, z.length(), z.op, z[12], z[54], z.angle(new Vector2(-3, -4)), z.angle(-6, -8));
     //console.log('FPS: ' + 1 / delta);
     sw.update = null;
